4f1b712755d93ed128e8deb676f2b3932ea33bb5
Sunilchow19 <you@example.com>
2025-01-27 13:02:08 +0530

    Port added to .env file

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -180,6 +180,6 @@ app.post('/send-feedback', async (req, res) => {
 });
 
 // Listen on port
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
